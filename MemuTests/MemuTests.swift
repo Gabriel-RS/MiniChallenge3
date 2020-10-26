301ace1f2774c3481ec5a13ed0192163b8181e1a
Gabriel Rodrigues <gabrielrsenac@gmail.com>
2020-10-26 15:12:31 -0300

    Initial Commit

---
+++ MemuTests/MemuTests.swift
@@ -0,0 +1,33 @@
+//
+//  MemuTests.swift
+//  MemuTests
+//
+//  Created by Gabriel Rodrigues da Silva on 26/10/20.
+//
+
+import XCTest
+@testable import Memu
+
+class MemuTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
