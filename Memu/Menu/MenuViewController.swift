2474055538c077533815c32f17499e37d0cf21bd
Gabriel Rodrigues <gabrielrsenac@gmail.com>
2020-11-03 15:14:28 -0300

    Files

---
+++ Memu/Menu/MenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuViewController.swift
+//  Memu
+//
+//  Created by Gabriel Rodrigues da Silva on 03/11/20.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Memu/Puzzle/PuzzleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PuzzleViewController.swift
+//  Memu
+//
+//  Created by Gabriel Rodrigues da Silva on 03/11/20.
+//
+
+import UIKit
+
+class PuzzleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
