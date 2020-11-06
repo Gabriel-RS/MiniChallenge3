6949360a501591ad15ccd1c50d725f15d8abc794
Felipe Henriques <felipeferreirahenriques@outlook.com>
2020-11-06 17:28:00 -0300

    Notas salvas em um vetor. Não é possivel clicar em mais de 4 notas

--- Memu/Notes/NotasViewController.swift
+++ Memu/Notes/NotasViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class Notas: UIViewController {
     
+    var aux:[String] = ["do", "re", "mi", "fa"]
     @IBOutlet weak var btnDo: UIButton!
     @IBOutlet weak var btnRe: UIButton!
     @IBOutlet weak var btnMi: UIButton!
@@ -18,45 +19,73 @@ class Notas: UIViewController {
     @IBOutlet weak var btnSol: UIButton!
     @IBOutlet weak var btnLa: UIButton!
     @IBOutlet weak var btnSi: UIButton!
+    @IBOutlet weak var lblAviso: UILabel!
     
     
     
     @IBAction func btnDo(_ sender: UIButton) {
-        selectedOrNot(btn: btnDo)
+        selectedOrNot(btn: btnDo, nota: "do")
     }
     
     @IBAction func btnRe(_ sender: Any) {
-        selectedOrNot(btn: btnRe)
+        selectedOrNot(btn: btnRe, nota: "re")
     }
     
     
     @IBAction func btnMi(_ sender: Any) {
-        selectedOrNot(btn: btnMi)
+        selectedOrNot(btn: btnMi, nota: "mi")
     }
     
     @IBAction func btnFa(_ sender: Any) {
-        selectedOrNot(btn: btnFa)
+        selectedOrNot(btn: btnFa, nota: "fa")
     }
     
     
     @IBAction func btnSol(_ sender: Any) {
-        selectedOrNot(btn: btnSol)
+        selectedOrNot(btn: btnSol, nota: "sol")
     }
     
     @IBAction func btnLa(_ sender: Any) {
-        selectedOrNot(btn: btnLa)
+        selectedOrNot(btn: btnLa, nota: "la")
     }
     
     @IBAction func btnSi(_ sender: Any) {
-        selectedOrNot(btn: btnSi)
+        selectedOrNot(btn: btnSi, nota: "si")
     }
     
-    func selectedOrNot(btn: UIButton){
-        if(btn.isSelected){
-            btn.isSelected = false
-        } else {
-            btn.isSelected = true
+    @IBAction func btnInprime(_ sender: Any) {
+        for i in 0...aux.count-1{
+            print(aux[i])
         }
     }
     
+    func selectedOrNot(btn: UIButton, nota: String){
+        if(aux.count == 4) {
+        lblAviso.isHidden = true
+            if(btn.isSelected){
+                btn.isSelected = false
+                for i in 0...aux.count-1 {
+                    if(aux[i] == nota) {
+                        aux.remove(at: i)
+                    }
+                }
+            } else {
+                lblAviso.isHidden = false
+            }
+        } else if (aux.count < 4){
+            if(btn.isSelected == false) {
+                btn.isSelected = true
+                aux.append(nota)
+            }
+        } else {
+            btn.isSelected = false
+            lblAviso.isHidden = false
+        }
+    }
+
+    
 }
+
+
+
+
