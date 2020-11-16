77845a5d44435e4f33b08b3593ad5e7129d95309
Douglas Cardoso <douglascf12@live.com>
2020-11-16 18:04:48 -0300

    tela criada

--- Memu/Rewards/RewardsViewController.swift
+++ Memu/Rewards/RewardsViewController.swift
@@ -8,22 +8,73 @@
 import UIKit
 
 class RewardsViewController: UIViewController {
+    
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var ivImage: UIImageView!
+    @IBOutlet weak var lbDescription: UILabel!
+    @IBOutlet weak var pvPlayer: UIProgressView!
+    
+    @IBOutlet weak var lbScore: UILabel!
+    
+    @IBOutlet weak var pvProgress: UIProgressView!
+    
+    var nivel: Float = 3.0
+    var count: Float = 0.0
+    var score: Float = 0.0
+    
+    var bronze: Bool = true
+    var prata: Bool = false
+    var ouro: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        progressPlayer()
+        //styleProgress()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func progressPlayer() {
+        pvPlayer.transform = CGAffineTransform(scaleX: 1.0, y: 5.0)
+        pvPlayer.setProgress(score, animated: true)
+        perform(#selector(updateProgressPlayer), with: nil, afterDelay: 1.0)
     }
-    */
+    
+    func styleProgress() {
+        pvProgress.transform = CGAffineTransform(scaleX: 1.0, y: 5.0)
+        pvProgress.setProgress(count, animated: true)
+        perform(#selector(updateProgress), with: nil, afterDelay: 1.0)
+    }
+    
+    @objc func updateProgressPlayer() {
+        //count = count + 1.0
+        pvPlayer.progress = score/150
+        lbScore.text = "\(Int(score))/\(150)"
+        
+//        if score == 10 {
+//            self.pvPlayer.progressImage = UIImage(named: "keyGreenOn")
+//        }
+        
+        if score <= 150 {
+            perform(#selector(updateProgressPlayer), with: nil, afterDelay: 1.0)
+            self.score+=1.0
+            
+        }
+    }
+    
+    @objc func updateProgress() {
+        //count = count + 1.0
+        pvProgress.progress = count/nivel
+        
+        if count <= nivel {
+            perform(#selector(updateProgress), with: nil, afterDelay: 1.0)
+            self.count+=1.0
+            
+        } else if count > nivel && count <= nivel{
+            self.nivel = 4
+            self.count = 0
+            pvProgress.progress = 0
+            perform(#selector(updateProgress), with: nil, afterDelay: 1.0)
+        }
+    }
+    
 
 }
