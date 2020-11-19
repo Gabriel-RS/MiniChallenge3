e1a2b663c2d278ca160df98cfdba3eb83954f948
Douglas Cardoso <douglascf12@live.com>
2020-11-19 17:19:26 -0300

    botão obter recompensa add

--- Memu/Rewards/RewardsViewController.swift
+++ Memu/Rewards/RewardsViewController.swift
@@ -12,54 +12,73 @@ class RewardsViewController: UIViewController {
     @IBOutlet weak var lbTitle: UILabel!
     @IBOutlet weak var ivImage: UIImageView!
     @IBOutlet weak var lbDescription: UILabel!
-    @IBOutlet weak var progressPlayer: UIProgressView!
+    @IBOutlet weak var pvPlayer: UIProgressView!
     @IBOutlet weak var btProgressPlayer: UIButton!
     @IBOutlet weak var lbScore: UILabel!
     @IBOutlet weak var lbProgress: UILabel!
-    @IBOutlet weak var pvProgress: UIProgressView!
+    @IBOutlet weak var pvDo: UIProgressView!
+    @IBOutlet weak var btRewardDo: UIButton!
+    
+    
     
     let titles: [String] = ["Desconhecido", "Explorador", "Músico amador", "Mestre dos sons", "Deus da música"]
     
     var level: Int = 2
     var pointsToLevelUp: Float = 0
     //var count: Float = 0.0
-    var score: Float = 0.0
+    var points: Float = 245.0
+    var noteDo: Float = 3.0
+    var levelNoteDo: Float = 3.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        progressPlayer.transform = CGAffineTransform(scaleX: 1.0, y: 5.0)
-        progressPlayer.setProgress(score, animated: true)
+        pvPlayer.transform = CGAffineTransform(scaleX: 1.0, y: 5.0)
+        pvPlayer.setProgress(points, animated: true)
         
-        pvProgress.transform = CGAffineTransform(scaleX: 1.0, y: 5.0)
-        pvProgress.setProgress(score, animated: true)
-        pvProgress.progress = 1/3
+        pvDo.transform = CGAffineTransform(scaleX: 1.0, y: 5.0)
+        pvDo.setProgress(points, animated: true)
+        pvDo.progress = noteDo/levelNoteDo
+        btRewardDo.isHidden = true
         
         if level == 0 || level == 1 {
             self.pointsToLevelUp = 100.0
         } else if level == 2 {
-            self.pointsToLevelUp = 150.0
+            self.pointsToLevelUp = 250.0
         } else if level == 3 {
-            self.pointsToLevelUp = 200.0
+            self.pointsToLevelUp = 450.0
         }
         
-        if pointsToLevelUp == score {
-            btProgressPlayer.setImage(UIImage(named: "progressoOuro"), for: .normal)
-        }
+//        if pointsToLevelUp == score {
+//            btProgressPlayer.setImage(UIImage(named: "progressoOuro"), for: .normal)
+//        }
+        checkProgressPlayer()
         
-        
-        test()
+        progressPlayer()
         refreshPage(level)
         
         
         //updateProgress()
     }
     
-    func test() {
-        if score <= pointsToLevelUp {
-            progressPlayer.progress = score/pointsToLevelUp
+    func progressPlayer() {
+        if points <= pointsToLevelUp {
+            pvPlayer.progress = points/pointsToLevelUp
         }
-        lbScore.text = "\(Int(score))/\(Int(pointsToLevelUp))"
+        lbScore.text = "\(Int(points))/\(Int(pointsToLevelUp))"
+    }
+    
+    func checkProgressPlayer() {
+        if pointsToLevelUp == points {
+            btProgressPlayer.setImage(UIImage(named: "progressoOuro"), for: .normal)
+        }
+        
+        if noteDo >= levelNoteDo {
+            pvDo.isHidden = true
+            btRewardDo.isHidden = false
+            lbProgress.text = "Clique no botão para obter sua recompesa"
+        }
+        
     }
     
 //    func updateProgress() {
@@ -110,6 +129,7 @@ class RewardsViewController: UIViewController {
 //    }
     
     func refreshPage(_ level: Int) {
+        
         switch level {
         case 0:
             lbTitle.text = "\(titles[level])"
@@ -143,8 +163,17 @@ class RewardsViewController: UIViewController {
     
     @IBAction func btCheckProgress(_ sender: Any) {
         if level != 0 && level != 4  {
-            lbProgress.text = "Faltam \(Int(pointsToLevelUp-score)) para você conquistar o título de \(titles[level+1])."
+            lbProgress.text = "Faltam \(Int(pointsToLevelUp-points)) para você conquistar o título de \(titles[level+1])."
         }
     }
+    
+    @IBAction func rewardDo(_ sender: Any) {
+        points+=10
+        btRewardDo.isHidden = true
+        pvDo.isHidden = false
+        //refreshPage(level)
+        progressPlayer()
+    }
+    
 
 }
