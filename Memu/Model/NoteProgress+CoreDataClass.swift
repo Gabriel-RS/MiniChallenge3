93091808158cf4ba94f4dc0cb6d9bf330aff5c87
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2020-11-20 18:30:31 -0300

    Adiciona entidades referente a nota e player

---
+++ Memu/Model/NoteProgress+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  NoteProgress+CoreDataClass.swift
+//  Memu
+//
+//  Created by Beatriz Sato on 20/11/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NoteProgress)
+public class NoteProgress: NSManagedObject {
+
+}
---
+++ Memu/Model/PlayerProgress+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerProgress+CoreDataClass.swift
+//  Memu
+//
+//  Created by Beatriz Sato on 20/11/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PlayerProgress)
+public class PlayerProgress: NSManagedObject {
+
+}
